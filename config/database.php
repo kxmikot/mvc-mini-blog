dd0ce35890b9110c70f8ffb796669c99c3f341d7
KXMIKOTO <kxmikoto@gmail.com>
2025-09-25 00:20:48 +0300

    +: added sqlite database

---
+++ config/database.php
@@ -0,0 +1,26 @@
+<?php
+
+$databasePath = __DIR__ . '/../database/database.db';
+
+try {
+    $pdo = new PDO('sqlite:' . $databasePath);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo.exec("
+        CREATE TABLE IF NOT EXIST users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            user_password TEXT NOT NULL
+        );
+        CREATE TABLE IF NOT EXIST notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL,
+            FOREIGN KEY(user_id) REFERENCRS users(id)
+        );
+    ");
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
